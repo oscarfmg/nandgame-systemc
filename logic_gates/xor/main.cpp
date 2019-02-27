eb20f1e47f5b453b2e8388f345cfb6efc6012f09
Mendoza Gutiérrez, Oscar Francisco <oscarfmg@outlook.com>
2019-02-26 19:29:35 -0800

    Level4: Completed template Xor

--- logic_gates/xor/main.cpp
+++ logic_gates/xor/main.cpp
@@ -15,14 +15,33 @@ int sc_main(int argc, char* argv[]) {
   sc_signal<bool> a;
   sc_signal<bool> b;
   sc_signal<bool> o;
-
   xorbase.a(a);
   xorbase.b(b);
   xorbase.o(o);
+
+  /* XorBaseW */
+  sc_vector<sc_signal<bool>> basew_inputs{"basew_in",3};
+  sc_signal<bool> basew_out{"basew_out"};
+  XorBaseW<3> xor3{"xor3"};
+  xor3.inputs.bind(basew_inputs);
+  xor3.output.bind(basew_out);
+
+  /* Xor<W> ; W=2 */
+  sc_vector<sc_signal<bool>> xor2_inputs{"xor2_in",2};
+  sc_signal<bool> xor2_out{"xor2_out"};
+  Xor<2> xor2{"Xor2"};
+  xor2.inputs.bind(xor2_inputs);
+  xor2.output.bind(xor2_out);
   
-//  Xor<2> xor2{"Xor2"};
-//  Xor<3> xor3{"Xor3"};
- 
+  /* Xor<W> ; W=4 */
+  sc_vector<sc_signal<bool>> xor4_inputs{"xor4_in",4};
+  sc_signal<bool> xor4_out{"xor4_out"};
+  Xor<4> xor4{"Xor4"};
+  xor4.inputs.bind(xor4_inputs);
+  xor4.output.bind(xor4_out);
+
+
+  /* XorBase2 */
   auto print_xor = [&](){
     std::string s = fmt::format("@ {:>5} :: {} (A: {}, B: {}, Out: {:>5})\n",
                                 sc_time_stamp().to_string(),
@@ -33,10 +52,80 @@ int sc_main(int argc, char* argv[]) {
     std::cout << s;
   };
 
+  std::cout << "\n*** Testing XorBase2 ***" << std::endl;
   a.write(false); b.write(false); run_ns(1);  print_xor();
   a.write(false); b.write( true); run_ns(1);  print_xor();
   a.write( true); b.write(false); run_ns(1);  print_xor();
   a.write( true); b.write( true); run_ns(1);  print_xor();
+
+  /* XorBaseW -- W = 3*/
+  const int W3 = 3;
+  sc_uint<W3+1> tmp;
+  auto print_xor_w3 = [&](){
+    std::string s = fmt::format("@ {:>5} :: {} (A: {}, B: {}, C: {}, Out: {:>5})\n",
+                                sc_time_stamp().to_string(),
+                                xor3.name(),
+                                tmp[2],
+                                tmp[1],
+                                tmp[0],
+                                basew_out.read());
+    std::cout << s;
+  };
+  
+  std::cout << "\n*** Testing XorBaseW ***" << std::endl;
+  for (tmp = 0; tmp <= max2pow(W3); ++tmp) {
+    for (int i = 0; i < W3; ++i) {
+      basew_inputs[W3-i-1].write(tmp[i]);
+    }
+    run_ns(1);
+    print_xor_w3();
+  }
+
+  /* Xor<2> */
+  const int W2 = 2;
+  sc_uint<W2+1> tmp2;  
+  auto print_xor2 = [&] () {
+    std::string s = fmt::format("@ {:>5} :: {} (A: {}, B: {}, Out: {:>5})\n",
+                                sc_time_stamp().to_string(),
+                                xor2.name(),
+                                tmp2[1],
+                                tmp2[0],
+                                xor2_out.read());
+    std::cout << s;
+  };
+  
+  std::cout << "\n*** Testing Xor<2> ***" << std::endl;
+  for (tmp2 = 0; tmp2 <= max2pow(W2); ++tmp2) {
+    for (int i = 0; i < W2; ++i) {
+      xor2_inputs[W2-i-1].write(tmp2[i]);
+    }
+    run_ns(1);
+    print_xor2();
+  }
  
-  return 0;
+  /* Xor<W> ; W=4*/
+  const int W4 = 4;
+  sc_uint<W4+1> tmp4;
+  auto print_xor_w4 = [&](){
+    std::string s = fmt::format("@ {:>5} :: {} (A: {}, B: {}, C: {}, D: {}, Out: {})\n",
+                                sc_time_stamp().to_string(),
+                                xor4.name(),
+                                tmp4[3],
+                                tmp4[2],
+                                tmp4[1],
+                                tmp4[0],
+                                static_cast<int>(xor4_out.read()));
+    std::cout << s;
+  };
+  
+  std::cout << "\n*** Testing Xor<4> ***" << std::endl;
+  for (tmp4 = 0; tmp4 <= max2pow(W4); ++tmp4) {
+    for (int i = 0; i < W4; ++i) {
+      xor4_inputs[W4-i-1].write(tmp4[i]);
+    }
+    run_ns(1);
+    print_xor_w4();
+  }
+
+return 0;
 }
--- logic_gates/xor/xor.h
+++ logic_gates/xor/xor.h
@@ -3,6 +3,7 @@
 
 #include <systemc>
 #include <type_traits>
+#include <fmt/format.h>
 using namespace sc_core;
 
 class XorBase2 : public sc_module {
@@ -14,14 +15,31 @@ public:
   XorBase2(sc_module_name);
 private:
   void do_xor();
+  void print_xor();
 };
+
+void XorBase2::print_xor() {
+  std::string s = fmt::format("@ {:>5} :: {} (A: {}, B: {}, Out: {:>5})\n",
+                              sc_time_stamp().to_string(),
+                              name(),
+                              static_cast<int>(a.read()),
+                              static_cast<int>(b.read()),
+                              o.read());
+//  std::cout << s;
+}
 
 XorBase2::XorBase2(sc_module_name name)
 : sc_module(name)
+, a{"a"}
+, b{"b"}
+, o{"o"}
 {
   SC_HAS_PROCESS(XorBase2);
   SC_METHOD(do_xor);
   sensitive << a << b;
+
+  SC_METHOD(print_xor);
+  sensitive << o;
 }
 
 void XorBase2::do_xor() {
@@ -45,23 +63,84 @@ XorBaseW<W>::XorBaseW(sc_module_name name)
 : sc_module(name)
 , m_xors{"xor",W-1}
 , m_inner_sigs{"signal_next_xor",W-2}
+, inputs{"input",W}
+, output{"output"}
 {
+  m_xors[0].a(inputs[0]);
+  m_xors[0].b(inputs[1]);
+ 
+  int inner_signal = 0;
+  for (;inner_signal < W-2; ++inner_signal) {
+    m_xors[inner_signal+1].a(m_inner_sigs[inner_signal]);
+    m_xors[inner_signal].o(m_inner_sigs[inner_signal]);
+
+    m_xors[inner_signal+1].b(inputs[inner_signal+2]);
+  }
+
+  m_xors[W-2].o(output);
   static_assert(W > 2, "Module width must be greather than 1.");
 }
+
+
+
 
 template <int W>
 class Xor : public std::conditional<(W>2),XorBaseW<W>,XorBase2>::type {
 public:
-
+  sc_vector<sc_in<bool>> inputs;
+  sc_out<bool> output;
   Xor(sc_module_name);
-private:
+  //private:
+  void print_this();
 };
 
+template<>
+void Xor<2>::print_this() {
+//  std::cout << a.read() << b.read() << o.read() << std::endl;
+}
+
+template<int W>
+void Xor<W>::print_this() {
+}
+/*template <int W>
+Xor<W>::Xor(sc_module_name name)
+: std::conditional<(W>2),XorBaseW<W>,XorBase2>::type(name)
+{
+}*/
+
+template <>
+Xor<2>::Xor(sc_module_name name)
+: XorBase2(name)
+,inputs{"inputs",2}
+,output{"output"}
+{
+  a(inputs[0]);
+  b(inputs[1]);
+  o(output);
+
+  SC_HAS_PROCESS(Xor);
+
+  SC_METHOD(print_this);
+  sensitive << a << b << o;
+}
 
 template <int W>
 Xor<W>::Xor(sc_module_name name)
-//: (name)
-: std::conditional<(W>2),XorBaseW<W>,XorBase2>::type(name)
-{
+: XorBaseW<W>(name)
+, inputs{"intput",W}
+, output{"output"} {
+  XorBaseW<W>::inputs.bind(inputs);
+  XorBaseW<W>::output.bind(output);
+  SC_HAS_PROCESS(Xor);
+  SC_METHOD(print_this);
 }
+
+/*template<>
+void Xor<2>::print_this() {
+}
+
+template<int W>
+void Xor<W>::print_this() {
+}*/
+
 #endif // XOR_XOR_H_
